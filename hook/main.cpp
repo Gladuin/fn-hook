ff03a982742c71565dd92fad325559f1fb7b13ed
Gladuin <34450360+Gladuin@users.noreply.github.com>
2022-06-12 16:20:34 +0200

    add return values for hook functions

--- hook/main.cpp
+++ hook/main.cpp
@@ -8,11 +8,11 @@ typedef int64_t (__fastcall *osdfunc_p)(uint16_t);
 typedef int64_t (__fastcall *runfunc_p)(LPVOID);
 
 int64_t __fastcall osdfunc_hook(uint16_t resource_id) {
-
+	return 0;
 }
 
 int64_t __fastcall runfunc_hook(LPVOID lpThreadParameter) {
-
+	return 0;
 }
 
 
