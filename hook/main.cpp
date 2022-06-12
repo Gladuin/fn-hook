02d90a22de1fe9c80ace0ba36a41df7921659b1e
Gladuin <34450360+Gladuin@users.noreply.github.com>
2022-06-12 14:47:29 +0200

    add hooked functions' skeleton code

--- hook/main.cpp
+++ hook/main.cpp
@@ -1,6 +1,19 @@
 #include <Windows.h>
 #include <MinHook.h>
 
+#include <cstdint>
+
+typedef int64_t (__fastcall *osdfunc_p)(uint16_t);
+typedef int64_t (__fastcall *runfunc_p)(LPVOID);
+
+int64_t __fastcall osdfunc_hook(uint16_t resource_id) {
+
+}
+
+int64_t __fastcall runfunc_hook(LPVOID lpThreadParameter) {
+
+}
+
 BOOL WINAPI DllMain(HMODULE handle, DWORD reason, LPVOID reserved) {
 	switch (reason) {
 		case DLL_PROCESS_ATTACH:
