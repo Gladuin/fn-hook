213ae7c5d774b91e90284e2ceb1972cff888a7ba
Gladuin <34450360+Gladuin@users.noreply.github.com>
2022-06-13 19:42:43 +0200

    add better minhook error handling

--- hook/main.cpp
+++ hook/main.cpp
@@ -2,46 +2,60 @@
 #include <MinHook.h>
 
 #include <cstdint>
+#include <string>
 
 
 typedef int64_t (__fastcall *osdfunc_p)(uint16_t);
 typedef int64_t (__fastcall *runfunc_p)(LPVOID);
 
 int64_t __fastcall osdfunc_hook(uint16_t resource_id) {
-	return 0;
+    return 0;
 }
 
 int64_t __fastcall runfunc_hook(LPVOID lpThreadParameter) {
-	return 0;
+    return 0;
 }
 
 
-DWORD WINAPI hook_init() {
-	uint64_t base_address = (uint64_t)GetModuleHandle(L"FnHotkeyUtility.exe");
+void check_mh_error(MH_STATUS status, std::string mh_type, HMODULE dll_handle) {
+    if (status != MH_OK) {
+        std::string msgbox_str = "Error " + std::string(MH_StatusToString(status)) + " occurred in function " + mh_type;
+        MessageBoxA(NULL, msgbox_str.c_str(), "MinHook error", MB_OK | MB_ICONERROR);
 
-	int error_code = MH_Initialize();
-		error_code |= MH_CreateHook((osdfunc_p)(base_address + 0x12070), osdfunc_hook, 0);
-		error_code |= MH_CreateHook((runfunc_p)(base_address + 0x12510), runfunc_hook, 0);
-		error_code |= MH_EnableHook(MH_ALL_HOOKS);
+        FreeLibraryAndExitThread(dll_handle, EXIT_FAILURE);
+    }
+}
 
-	if (error_code != MH_OK) {
-		MessageBox(NULL, L"One or more errors were raised while initialising MinHook!", L"MinHook Error", MB_OK | MB_ICONERROR);
-		return EXIT_FAILURE;
-	}
+DWORD WINAPI hook_init(LPVOID dll_handle) {
+    uint64_t base_address = (uint64_t)GetModuleHandle(L"FnHotkeyUtility.exe");
 
-	return EXIT_SUCCESS;
+    check_mh_error(MH_Initialize(),
+                   "MH_Initialize",
+                   (HMODULE)dll_handle);
+    check_mh_error(MH_CreateHook((osdfunc_p)(base_address + 0x12070), osdfunc_hook, 0),
+                   "MH_CreateHook (osdfunc)",
+                   (HMODULE)dll_handle);
+    check_mh_error(MH_CreateHook((runfunc_p)(base_address + 0x12510), runfunc_hook, 0),
+                   "MH_CreateHook (runfunc)",
+                   (HMODULE)dll_handle);
+    check_mh_error(MH_EnableHook(MH_ALL_HOOKS),
+                   "MH_EnableHook",
+                   (HMODULE)dll_handle);
+
+    return EXIT_SUCCESS;
 }
 
 BOOL WINAPI DllMain(HMODULE handle, DWORD reason, LPVOID reserved) {
-	switch (reason) {
-		case DLL_PROCESS_ATTACH:
-			CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)hook_init, NULL, NULL, NULL);
-			break;
+    switch (reason) {
+        case DLL_PROCESS_ATTACH:
+            CreateThread(NULL, NULL, hook_init, handle, NULL, NULL);
+            break;
 
-		case DLL_PROCESS_DETACH:
-			MH_Uninitialize();
-			break;
-	}
+        case DLL_PROCESS_DETACH:
+            MH_DisableHook(MH_ALL_HOOKS);
+            MH_Uninitialize();
+            break;
+    }
 
-	return TRUE;
+    return TRUE;
 }
